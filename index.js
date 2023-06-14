0261a65e44ca57d9c8c4cef333afdf21c0517318
NE21-sekine <ne211072@sensgu-u.jp>
2023-06-14 10:18:06 +0900

    search 作成

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = 8082;
+
 class Product {
     constructor(id, name, price) {
       this.id = id;
@@ -23,3 +27,30 @@ let foods = new ProductDB();
 foods.add("ポッキー", 280);
 foods.add("きのこの山", 190);
 foods.add("たけのこの里", 190);
+
+app.get('/', (req, res) => {
+    res.contentType('json');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send({result:foods.length, data:foods});
+});
+
+app.get('/request', (req, res) => {
+    if (req.query.cmd == "search"){
+        let result = { err: "", result: 0, data: [] };
+        res.contentType('json');
+        res.header('Access-Control-Allow-Origin', '*');
+        if (req.query.name != null){
+            foods.products.forEach(e => {
+                if (e.name == req.query.name) {
+                    result.result++;
+                    result.data.push(e);
+                }
+            });
+        }else {
+            result.err = "seach name not specified"
+        }
+        res.send(result);
+    }
+});
+
+app.listen(process.env.PORT || PORT);
